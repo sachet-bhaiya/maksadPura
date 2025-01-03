3218a9fea1ea7798825d44f8d6711a474e7a14d5
ms32-org <ms32-org@outlook.com>
2025-01-03 10:33:12 +0530

    all delte self destruct

--- remoteside/hide.pyw
+++
@@ -1,4 +0,0 @@
-from win32 import win32gui
-
-taskbar_hwnd = win32gui.FindWindow("Shell_TrayWnd", None)
-win32gui.ShowWindow(taskbar_hwnd, 0)
--- rotate.py
+++
@@ -1,6 +0,0 @@
-import unicodedata
-
-# ASCII code for '\x10'
-char = chr(16)  # Equivalent to '\x10'
-name = unicodedata.name(char, f"Non-printable ASCII {ord(char)}")
-print(name)  # Output: 'DEVICE CONTROL TWO'
